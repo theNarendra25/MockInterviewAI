3dfb0af58fd7a33f3172b25ca043dc60ce475e64
Narendra Kumar Meena <152044653+theNarendra25@users.noreply.github.com>
2024-07-04 02:23:58 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: 'postgresql://AI-Interview_owner:b8jMZOrFCmA5@ep-lucky-pond-a1f1ha95.ap-southeast-1.aws.neon.tech/AI-Interview?sslmode=require',
+      url: 'postgress_URL',
     }
   };
